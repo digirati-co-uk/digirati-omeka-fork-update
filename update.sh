b6c4e069a950a37737d42c255c8694bc5521383e
Ross McFadyen <ross.mcfadyen@digirati.com>
2017-05-25 20:29:58 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-curl -i -H 'Authorization: token ${GH_TOKEN}' /
-		-H "Content-Type: application/json" /
-		-X POST -d '{"title":"weekly PR","head":"omeka:develop","base":"develop","body":"weekly PR","maintainer_can_modify":true}' /
-		https://api.github.com/repos/digirati-co-uk/omeka-s/pulls
+curl -i -H 'Authorization: token ${GH_TOKEN}' -H "Content-Type: application/json" -X POST -d '{"title":"weekly PR","head":"omeka:develop","base":"develop","body":"weekly PR","maintainer_can_modify":true}' https://api.github.com/repos/digirati-co-uk/omeka-s/pulls

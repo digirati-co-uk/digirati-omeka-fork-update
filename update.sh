0ee7c05f3b06e40d25d6ae22d5e1920e8338f95a
Ross McFadyen <ross.mcfadyen@digirati.com>
2017-05-25 20:27:55 +0100

    initial commit

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -i -H 'Authorization: token ${GH_TOKEN}' /
+		-H "Content-Type: application/json" /
+		-X POST -d '{"title":"weekly PR","head":"omeka:develop","base":"develop","body":"weekly PR","maintainer_can_modify":true}' /
+		https://api.github.com/repos/digirati-co-uk/omeka-s/pulls
